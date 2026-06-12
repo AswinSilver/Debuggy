6ea0d884391bcfad5aae8199e55f329304ccc85a
aswinraj <awinraj123as@gmail.com>
2026-06-12 19:36:23 +0530

    added new api key

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+import os
 
 app = Flask(__name__)
 CORS(app)
-
-GROQ_API_KEY = "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"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_URL = "https://api.groq.com/openai/v1/chat/completions"
 
 def call_groq(prompt):
