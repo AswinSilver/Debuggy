486ef6357a51dbed7352b89f15142c639b81c261
aswinraj <awinraj123as@gmail.com>
2026-06-12 19:46:32 +0530

    updated js

--- frontend/script.js
+++ frontend/script.js
@@ -69,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
         fixOutput.value = "";
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/debug", {
+            const response = await fetch("https://debuggy.onrender.com", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ code, language: selectedLanguage })
